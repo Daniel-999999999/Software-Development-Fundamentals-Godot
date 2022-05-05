745ff5335b498b17745292bd922da262308587b2
krishna <0186359@schoolsnet.act.edu.au>
2022-05-05 11:55:12 +1000

    cxz
    cxz

--- MainGame/MainGame.gd
+++ MainGame/MainGame.gd
@@ -15,6 +15,19 @@ func _ready():
 		currentTimer = currentTimer -1
 		print(currentTimer)
 	print("GAME OVER")
+	get_tree().change_scene("res://lose screen/Lose Screen.tscn")
+	
+	while currentTimer > 0:
+		yield (get_tree().create_timer(1.0), "timeout")
+		$HUD/Countdown.text = str(currentTimer)
+		currentTimer = currentTimer - 1
+		print(currentTimer)
+	print("Game Over")
+	
+func _process(delta):
+	$HUD/CurrentScore.text = str(GlobalVariables.scoringInformation["currentScore"])
+	if get_tree().get_nodes_in_group("Enimie").size() == 0:
+		get_tree().change_scene("res://WinScreen.tscn")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
